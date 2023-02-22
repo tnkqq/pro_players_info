8c0457442f38bb0edf9d6c4b6cfa28e933eaa034
tnkqq <dtonenkov@mail.ru>
2023-02-22 03:11:00 +0300

    parser 2nd updt

--- pro_player_parser.py
+++ pro_player_parser.py
@@ -5,24 +5,30 @@ import time
 import lxml
 import requests
 
-def get_page (nickname):
+def get_page_info (nickname):
 
     url = f"https://prosettings.net/players/{nickname}/"
     src = requests.get(url).text
 
+
+    """player_bio_table""" 
+    
     try:
         soup =BeautifulSoup(src,"lxml")
         table=soup.find(class_= "player-bio").find(class_= "intro").find(class_="data").find_all("tr")
         player_table_dict =dict()
+        
         for tr in table:
             td = tr.find("td").text.strip()
             th = tr.find("th").text.strip()
-            player_table_dict [th] =td 
+            player_table_dict [th] = td 
 
         return player_table_dict
+
     except:
-        return("smthng wrong")
+        return("Player Bio was NOT FOUND!")
     
+    """player_gear"""
 
 
-print(get_page("s1mple"))
+    """player_pc_specs"""
