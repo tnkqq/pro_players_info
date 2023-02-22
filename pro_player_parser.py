dc9637b6c38d163f9c0bfe5d5936c12b10b7cb66
tnkqq <dtonenkov@mail.ru>
2023-02-22 13:43:45 +0300

    parser save

--- pro_player_parser.py
+++ pro_player_parser.py
@@ -5,14 +5,7 @@ import time
 import lxml
 import requests
 
-def get_page_info (nickname):
-
-    url = f"https://prosettings.net/players/{nickname}/"
-    src = requests.get(url).text
-
-
-    """player_bio_table""" 
-    
+def get_player_bio(src):
     try:
         soup =BeautifulSoup(src,"lxml")
         table=soup.find(class_= "player-bio").find(class_= "intro").find(class_="data").find_all("tr")
@@ -27,8 +20,47 @@ def get_page_info (nickname):
 
     except:
         return("Player Bio was NOT FOUND!")
+
+def get_player_gear(src):
+    try:
+        soup =BeautifulSoup(src,"lxml")
+        gear = soup.find(class_ = "center-container").find (class_ = "equipment").find(id = "gear").find(class_ = "cta-boxes--list").find_all(class_ ="cta-box promo linked ext-link promo linked ext-link")
+        gear_table_dict =dict()
+        for box_item in gear:
+            box_status = box_item.find(class_ = "cta-box_status-tag").text
+            gear_name = box_item.find("h4").find("a").text
+            
+            gear_table_dict [box_status] =gear_name
+            
     
-    """player_gear"""
 
+        return gear_table_dict
+    except:
+        return("Player Gear was NOT FOUND!")
 
-    """player_pc_specs"""
+def get_player_pc_specs(src):
+    soup =BeautifulSoup(src,"lxml")
+    specs = soup.find(class_ = "center-container").find (class_ = "equipment").find(id = "pcspecs").find(class_ = "cta-boxes--list").find_all(class_ ="cta-box promo linked ext-link promo linked ext-link")
+    
+    pc_specs_dict = dict()
+    for box_item in specs:
+        box_status = box_item.find(class_ = "cta-box_status-tag").text
+        specs_name = box_item.find("h4").find("a").text
+        pc_specs_dict [box_status] =specs_name
+    return pc_specs_dict
+
+    
+
+def get_page_info (nickname):
+
+    url = f"https://prosettings.net/players/{nickname}/"
+    src = requests.get(url).text
+
+    player_bio_dict = get_player_bio(src)
+    player_gear_dict = get_player_gear(src)
+    player_specs_dict = get_player_pc_specs(src)
+    return player_bio_dict,player_gear_dict,player_specs_dict
+    
+print(get_page_info("https://prosettings.net/players/s1mple/"))
+
+    
